fa793719593b2fb2f58625a9cbd36b03dc727c57
lukaszjambor <lukasz.jambor83@gmail.com>
2019-07-15 17:43:25 +0200

    new @nnotation here!! no need to use preauthorize and think what will be value with @CheckUser all you need is to put userId in controller method signature and rest will be done by code

---
+++ src/main/java/com/example2/demo/annotation/CheckUserAspect.java
@@ -0,0 +1,29 @@
+package com.example2.demo.annotation;
+
+import com.example2.demo.configurations.CustomUser;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.CodeSignature;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class CheckUserAspect {
+
+    @Around("@annotation(CheckUser)")
+    public Object checkUser(ProceedingJoinPoint joinPoint) throws Throwable {
+        CustomUser user = (CustomUser) (SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        CodeSignature codeSignature = ((CodeSignature) (joinPoint.getSignature()));
+        String[] parameterNames = codeSignature.getParameterNames();
+        int userIdIndex = Arrays.asList(parameterNames).indexOf("userId");
+        if (!user.getUserId().equals(joinPoint.getArgs()[userIdIndex])) {
+            throw new AccessDeniedException("no access");
+        }
+        return joinPoint.proceed();
+    }
+}
