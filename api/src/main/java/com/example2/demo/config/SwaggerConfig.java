3413db36d6ac1f836f3fd0c88fa406ae79c641d3
Łukasz <lukasz.jambor83@gmail.com>
2023-07-02 12:17:25 +0200

    add swagger 3 / open api 3 and basic config (future endpoints will get description in it with annotations

---
+++ api/src/main/java/com/example2/demo/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.example2.demo.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Games shop")
+                        .description("Spring games shop application")
+                        .version("v0.0.1"))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Project documentation will be under this link in future")
+                        .url("https://github.com/LukaszJambor/games"));
+    }
+}
